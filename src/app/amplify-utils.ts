7c8fe7d7f5be6e258f0817a3e53baf8dd4a8aee6
mshetty12 <mridulashetty3089@gmail.com>
2025-02-17 15:34:23 -0600

    Update amplify-utils.ts

--- src/app/amplify-utils.ts
+++ src/app/amplify-utils.ts
@@ -3,10 +3,43 @@ import { Schema } from "../../amplify/data/resource";
 import { Amplify } from "aws-amplify";
 import outputs from "../../amplify_outputs.json";
 import { configureAutoTrack } from 'aws-amplify/analytics';
+import { identifyUser } from 'aws-amplify/analytics';
+import { getCurrentUser } from 'aws-amplify/auth';
 
 Amplify.configure(outputs);
 
 export const amplifyClient = generateClient<Schema>();
+
+const location = {
+  latitude: 47.606209,
+  longitude: -122.332069,
+  postalCode: '98122',
+  city: 'Seattle',
+  region: 'WA',
+  country: 'USA'
+};
+
+const customProperties = {
+  plan: ['plan'],
+  phoneNumber: ['+11234567890'],
+  age: ['25']
+};
+
+const userProfile = {
+  location,
+  name: 'username',
+  email: 'name@example.com',
+  customProperties
+};
+
+async function sendUserData() {
+  const user = await getCurrentUser();
+
+  identifyUser({
+    userId: user.userId,
+    userProfile
+  });
+}
 
 configureAutoTrack({
   // REQUIRED, turn on/off the auto tracking
