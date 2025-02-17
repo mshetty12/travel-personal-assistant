a44f0b32269fa4bae16063c754273f95c62b1cfd
mshetty12 <mridulashetty3089@gmail.com>
2025-02-17 12:09:06 -0600

    Update amplify-utils.ts

--- src/app/amplify-utils.ts
+++ src/app/amplify-utils.ts
@@ -2,6 +2,7 @@ import { generateClient } from "aws-amplify/api";
 import { Schema } from "../../amplify/data/resource";
 import { Amplify } from "aws-amplify";
 import outputs from "../../amplify_outputs.json";
+import { configureAutoTrack } from 'aws-amplify/analytics';
 
 Amplify.configure(outputs);
 
@@ -19,18 +20,6 @@ configureAutoTrack({
     // OPTIONAL, the attributes of the event
     attributes: {
       customizableField: 'attr'
-    },
-    // OPTIONAL, the event name. By default, this is 'pageView'
-    eventName: 'pageView',
-
-    // OPTIONAL, the type of app under tracking. By default, this is 'multiPageApp'.
-    // You will need to change it to 'singlePage' if your app is a single-page app like React
-    appType: 'multiPageApp',
-
-    // OPTIONAL, provide the URL for the event.
-    urlProvider:  () => {
-      // the default function
-      return window.location.origin + window.location.pathname;
     }
   }
 });
