f573349b7d9d4553425891e2d7e6b66d1c178008
mshetty12 <mridulashetty3089@gmail.com>
2025-02-17 19:32:15 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,8 +20,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <script>
+          (function(n,i,v,r,s,c,x,z){x=window.AwsRumClient={q:[],n:n,i:i,v:v,r:r,c:c};window[n]=function(c,p){x.q.push({c:c,p:p});};z=document.createElement('script');z.async=true;z.src=s;document.head.insertBefore(z,document.head.getElementsByTagName('script')[0]);})(
+              'cwr',
+              'a0b0b239-afac-4252-bd8f-ecef00eb55cd',
+              '1.0.0',
+              'us-east-1',
+              'https://client.rum.us-east-1.amazonaws.com/1.19.0/cwr.js',
+              {
+                sessionSampleRate: 1,
+                identityPoolId: "us-east-1:22658177-1abf-4d80-b14a-e203cd5c5ec7",
+                endpoint: "https://dataplane.rum.us-east-1.amazonaws.com",
+                telemetries: ["performance","errors","http"],
+                allowCookies: true,
+                enableXRay: true
+              }
+            );
+      </script>
+      
       <body className={inter.className}>
-        <AuthWrapper>{children}</AuthWrapper>
+          <AuthWrapper>{children}</AuthWrapper>
       </body>
     </html>
   );
